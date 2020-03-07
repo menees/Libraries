01354f8f8a8343f0504766bd433a039859c04147
Bill Menees <Bill@Menees.com>
2020-03-07 08:41:40 -0600

    Working on Diffs.Tests.

--- tests/Menees.Diffs.Tests/DiffUtilityTests.cs
+++ tests/Menees.Diffs.Tests/DiffUtilityTests.cs
@@ -3,6 +3,9 @@ using Menees.Diffs;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SoftwareApproach.TestingExtensions;
+using System.IO;
+using System.Linq;
 
 namespace Menees.Diffs.Tests
 {
@@ -12,41 +15,70 @@ namespace Menees.Diffs.Tests
 		[TestMethod]
 		public void AreFilesDifferentTest()
 		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
-		}
+			using TempFile a1 = new TempFile("1234");
+			using TempFile a2 = new TempFile("1234");
+			using TempFile b = new TempFile("12345");
+			using TempFile c = new TempFile("12045");
 
-		[TestMethod]
-		public void AreFilesDifferentTest1()
-		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
+			static void Test(TempFile f1, TempFile f2, bool expected, string message)
+			{
+				DiffUtility.AreFilesDifferent(f1.FileName, f2.FileName).ShouldEqual(expected, message);
+				DiffUtility.AreFilesDifferent(f1.Info, f2.Info).ShouldEqual(expected, message);
+			}
+
+			Test(a1, a2, false, "a1==a2");
+			Test(a1, b, true, "a1!=b");
+			Test(a2, c, true, "a2!=c");
+			Test(b, c, true, "b!=c");
 		}
 
 		[TestMethod]
 		public void GetFileTextLinesTest()
 		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
+			using TempFile a = new TempFile("Line1");
+			using TempFile b = new TempFile("Line1\nLine2");
+			using TempFile c = new TempFile("Line1\r\nLine2\r\n");
+			using TempFile d = new TempFile("Line1\r\nLine2\r\nLine3");
+
+			static void TestLines(TempFile f, params string[] expected)
+			{
+				IList<string> actual = DiffUtility.GetFileTextLines(f.FileName);
+				CollectionAssert.AreEquivalent(expected, actual.ToArray());
+			}
+
+			TestLines(a, "Line1");
+			TestLines(b, "Line1", "Line2");
+			TestLines(c, "Line1", "Line2");
+			TestLines(d, "Line1", "Line2", "Line3");
 		}
 
 		[TestMethod]
 		public void GetStringTextLinesTest()
 		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
+			static void TestLines(string text, params string[] expected)
+			{
+				IList<string> actual = DiffUtility.GetStringTextLines(text);
+				CollectionAssert.AreEquivalent(expected, actual.ToArray());
+			}
+
+			TestLines("");
+			TestLines("1", "1");
+			TestLines("1\r\n2", "1", "2");
+			TestLines("1\r\n2\r\n3", "1", "2", "3");
 		}
 
 		[TestMethod]
 		public void GetTextLinesTest()
 		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
+			// The GetTextLines function is internally used by GetFileTextLines and GetStringTextLines, so other tests cover it too.
+			using TempFile f = new TempFile("Line1\r\nLine2\r\nLine3");
+			using TextReader reader = new StreamReader(f.FileName);
+			IList<string> actual = DiffUtility.GetTextLines(reader);
+			CollectionAssert.AreEquivalent(new[] { "Line1", "Line2", "Line3" }, actual.ToArray());
 		}
 
 		[TestMethod]
 		public void GetXmlTextLinesTest()
-		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
-		}
-
-		[TestMethod]
-		public void GetXmlTextLinesTest1()
 		{
 			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
 		}
@@ -60,13 +92,23 @@ namespace Menees.Diffs.Tests
 		[TestMethod]
 		public void IsBinaryFileTest()
 		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
-		}
+			using (TempFile t = new TempFile("This is text."))
+			{
+				DiffUtility.IsBinaryFile(t.FileName).ShouldBeFalse("text filename");
+				using (Stream s = File.OpenRead(t.FileName))
+				{
+					DiffUtility.IsBinaryFile(s).ShouldBeFalse("text stream");
+				}
+			}
 
-		[TestMethod]
-		public void IsBinaryFileTest1()
-		{
-			throw new NotImplementedException(); // TODO: Write test. [2/29/20]
+			using (TempFile t = new TempFile("Has\0embedded\0nulls."))
+			{
+				DiffUtility.IsBinaryFile(t.FileName).ShouldBeTrue("binary filename");
+				using (Stream s = File.OpenRead(t.FileName))
+				{
+					DiffUtility.IsBinaryFile(s).ShouldBeTrue("binary stream");
+				}
+			}
 		}
 	}
 }
---
+++ tests/Menees.Diffs.Tests/TempFile.cs
@@ -0,0 +1,73 @@
+﻿namespace Menees.Diffs.Tests
+{
+	#region Using Directives
+
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Text;
+
+	#endregion
+
+	internal sealed class TempFile : IDisposable
+	{
+		#region Private Methods
+
+		private bool disposed;
+
+		#endregion
+
+		#region Constructors
+
+		public TempFile(string content)
+			: this(content != null ? Encoding.UTF8.GetBytes(content) : null)
+		{
+		}
+
+		public TempFile(byte[] content)
+		{
+			this.FileName = Path.GetTempFileName();
+			if (content != null)
+			{
+				File.WriteAllBytes(this.FileName, content);
+			}
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		public string FileName { get; }
+
+		public FileInfo Info => new FileInfo(this.FileName);
+
+		#endregion
+
+		#region Public Methods
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					File.Delete(this.FileName);
+				}
+
+				disposed = true;
+			}
+		}
+
+		#endregion
+	}
+}
