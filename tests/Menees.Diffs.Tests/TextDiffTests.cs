d4d73a492a67cdc46a41e5e56b029c5fbbc6e69e
Bill Menees <Bill@Menees.com>
2020-03-07 17:00:28 -0600

    Added more TextDiffTests.

--- tests/Menees.Diffs.Tests/TextDiffTests.cs
+++ tests/Menees.Diffs.Tests/TextDiffTests.cs
@@ -11,45 +11,20 @@ namespace Menees.Diffs.Tests
 	public class TextDiffTests
 	{
 		[TestMethod]
-		public void ExecuteTest()
+		public void ExecuteGeneralTest()
 		{
-			static EditScript Diff(string left, string right, bool ignoreCase, bool ignoreOuterWhiteSpace, bool supportChangeEditType = true)
-			{
-				IList<string> leftLines = DiffUtility.GetStringTextLines(left);
-				IList<string> rightLines = DiffUtility.GetStringTextLines(right);
-
-				EditScript edits = null;
-				foreach (HashType hashType in Enum.GetValues(typeof(HashType)))
-				{
-					TextDiff diff = new TextDiff(hashType, ignoreCase, ignoreOuterWhiteSpace, 0, supportChangeEditType);
-					EditScript newEdits = diff.Execute(leftLines, rightLines);
-					if (edits != null)
-					{
-						edits.Count.ShouldEqual(newEdits.Count);
-						edits.TotalEditLength.ShouldEqual(newEdits.TotalEditLength);
-						edits.Similarity.ShouldEqual(newEdits.Similarity);
-					}
-
-					edits = newEdits;
-				}
-
-				return edits;
-			}
-
-			static void Check(EditScript edits, params EditType[] expectedEditTypes)
-			{
-				edits.Count.ShouldEqual(expectedEditTypes.Length, nameof(edits.Count));
-				for (int i = 0; i < expectedEditTypes.Length; i++)
-				{
-					Edit edit = edits[i];
-					edit.EditType.ShouldEqual(expectedEditTypes[i]);
-					edit.Length.ShouldEqual(1);
-				}
-			}
-
 			string a = "\tA\nB\nC\t";
 			string b = "\tA\nB\nD\t";
 			string c = "\tA\nB\nc\t\nD\t";
+
+			static void Check(EditScript edits, params EditType[] expectedEditTypes)
+			{
+				TextDiffTests.Check(edits, expectedEditTypes);
+				foreach (Edit edit in edits)
+				{
+					edit.Length.ShouldEqual(1);
+				}
+			}
 
 			Check(Diff(a, a, false, false));
 			Check(Diff(a, a.ToLower(), true, false));
@@ -62,6 +37,117 @@ namespace Menees.Diffs.Tests
 			Check(Diff(a, c, false, false), EditType.Change, EditType.Insert);
 			Check(Diff(a, c, true, false), EditType.Insert);
 			Check(Diff(b, c, true, false), EditType.Insert);
+		}
+
+		[TestMethod]
+		public void ExecuteDeleteTest()
+		{
+			string a = "1\n2\n3\n4";
+			string b = "1\n4";
+
+			EditScript edits = Diff(a, b, false, false, false);
+			Check(edits, EditType.Delete);
+			Edit edit = edits[0];
+			edit.Length.ShouldEqual(2);
+			edit.StartA.ShouldEqual(1);
+			edit.StartB.ShouldEqual(1);
+		}
+
+		[TestMethod]
+		public void ExecuteInsertTest()
+		{
+			string a = "1\n4";
+			string b = "1\n2\n3\n4";
+
+			EditScript edits = Diff(a, b, false, false, false);
+			Check(edits, EditType.Insert);
+			Edit edit = edits[0];
+			edit.Length.ShouldEqual(2);
+			edit.StartA.ShouldEqual(1);
+			edit.StartB.ShouldEqual(1);
+		}
+
+		[TestMethod]
+		public void ExecuteChangeTest()
+		{
+			string a = "1\n2\n3\n4";
+			string b = "1\nx\ny\n4";
+
+			EditScript edits = Diff(a, b, false, false, true);
+			Check(edits, EditType.Change);
+			Edit edit = edits[0];
+			edit.Length.ShouldEqual(2);
+			edit.StartA.ShouldEqual(1);
+			edit.StartB.ShouldEqual(1);
+		}
+
+		[TestMethod]
+		public void ExecuteInsertDeleteTest()
+		{
+			string a = "1\n2\n3\n4";
+			string b = "1\n3\n2\n4";
+
+			EditScript edits = Diff(a, b, false, false, false);
+			Check(edits, EditType.Insert, EditType.Delete);
+			Edit edit = edits[0];
+			edit.Length.ShouldEqual(1);
+			edit.StartA.ShouldEqual(1);
+			edit.StartB.ShouldEqual(1);
+			edit = edits[1];
+			edit.Length.ShouldEqual(1);
+			edit.StartA.ShouldEqual(2);
+			edit.StartB.ShouldEqual(3);
+		}
+
+		[TestMethod]
+		public void ExecuteDeleteInsertTest()
+		{
+			string a = "1\n2\n3";
+			string b = "1\nx\n3";
+
+			EditScript edits = Diff(a, b, false, false, false);
+			Check(edits, EditType.Delete, EditType.Insert);
+			Edit edit = edits[0];
+			edit.Length.ShouldEqual(1);
+			edit.StartA.ShouldEqual(1);
+			edit.StartB.ShouldEqual(1);
+			edit = edits[1];
+			edit.Length.ShouldEqual(1);
+			edit.StartA.ShouldEqual(1);
+			edit.StartB.ShouldEqual(1);
+		}
+
+		private static EditScript Diff(string left, string right, bool ignoreCase, bool ignoreOuterWhiteSpace, bool supportChangeEditType = true)
+		{
+			IList<string> leftLines = DiffUtility.GetStringTextLines(left);
+			IList<string> rightLines = DiffUtility.GetStringTextLines(right);
+
+			EditScript edits = null;
+			foreach (HashType hashType in Enum.GetValues(typeof(HashType)))
+			{
+				TextDiff diff = new TextDiff(hashType, ignoreCase, ignoreOuterWhiteSpace, 0, supportChangeEditType);
+				EditScript newEdits = diff.Execute(leftLines, rightLines);
+				if (edits != null)
+				{
+					edits.Count.ShouldEqual(newEdits.Count);
+					edits.TotalEditLength.ShouldEqual(newEdits.TotalEditLength);
+					edits.Similarity.ShouldEqual(newEdits.Similarity);
+				}
+
+				edits = newEdits;
+			}
+
+			return edits;
+		}
+
+		private static void Check(EditScript edits, params EditType[] expectedEditTypes)
+		{
+			edits.Count.ShouldEqual(expectedEditTypes.Length, nameof(edits.Count));
+			for (int i = 0; i < expectedEditTypes.Length; i++)
+			{
+				Edit edit = edits[i];
+				edit.EditType.ShouldEqual(expectedEditTypes[i]);
+			}
 		}
 	}
 }
