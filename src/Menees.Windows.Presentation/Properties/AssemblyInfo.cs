aafa97521692d96d64a3e353c35b7207cf3612b8
Bill Menees <Bill@Menees.com>
2020-03-15 10:05:56 -0500

    Fixed WPF ThemeInfo for default resource locations

--- src/Menees.Windows.Presentation/Properties/AssemblyInfo.cs
+++ src/Menees.Windows.Presentation/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Markup;
+﻿using System.Windows;
+using System.Windows.Markup;
+
+// Where theme-specific resource dictionaries are located (used if a resource is not found in the page, or application resource dictionaries)
+// Where the generic resource dictionary is located (used if a resource is not found in the page, app, or any theme specific resource dictionaries)
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 // This allows Xaml files in other projects to use xmlns:m="http://menees.com/xaml" instead of
 // xmlns:m="clr-namespace:Menees.Windows.Presentation;assembly=Menees.Windows.Presentation".
